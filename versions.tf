9d507229810e2f21a605d624abdf554d718a917d
Emerson Hardisty <emerson@hardisty.uk>
2026-07-14 13:55:49 +0100

    formatting

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.65.0"            # any 4.65.y, but not 4.66 or 5/6
+      version = "~> 4.65.0" # any 4.65.y, but not 4.66 or 5/6
     }
   }
 }
